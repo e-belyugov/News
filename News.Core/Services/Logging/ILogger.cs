9539c30a222a7716de1ed901e991ce983aa4d351
Evgeny Belyugov <e.belyugov@gmail.com>
2019-12-13 16:48:57 +0700

    23

---
+++ News.Core/Services/Logging/ILogger.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace News.Core.Services.Logging
+{
+    /// <summary>
+    /// Logger interface
+    /// </summary>
+    public interface ILogger
+    {
+        /// <summary>
+        /// Info logging
+        /// </summary>
+        void Info(string infoMessage);
+
+        /// <summary>
+        /// Error logging
+        /// </summary>
+        void Error(Exception exception);
+    }
+}

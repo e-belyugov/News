893b59e09be7aa0483ee65a77a52aad52a02d761
Evgeny Belyugov <e.belyugov@gmail.com>
2020-02-28 14:37:42 +0700

    28.02.2020 Vashgorod parser updated

--- News.Core/Services/Parsing/VashgorodParser.cs
+++ News.Core/Services/Parsing/VashgorodParser.cs
@@ -192,8 +192,8 @@ namespace News.Core.Services.Parsing
                                     articleItem.New = false;
 
                                 // Loading and parsing main source html
-                                string mainHtml = await _webService.GetDataAsync("https://www.facebook.com/vashgorod/",
-                                    Encoding.GetEncoding(parserData.SourceEncoding));
+                                //string mainHtml = await _webService.GetDataAsync("https://vk.com/vashgorod?own=0&count=20",
+                                //    Encoding.GetEncoding(parserData.SourceEncoding));
                             }
 
                             // Checking if article exists already
